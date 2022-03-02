520d7d9028e607bacf6a990233cc4ade30baa4e1
Denilson Morais <denilsonapmorais@gmail.com>
2022-03-02 13:04:50 -0300

    Update hashlinear.py

--- hashlinear.py
+++ hashlinear.py
@@ -1,25 +1,30 @@
 class Hashlinear:
     def __init__(self,x):
         self.chave = x
-        #print('O valor de x é: ',x)
+
 
     def funcaoHash(self):
         self.chaveHash = self.chave%4
-        #print(self.chave)
         return self.chaveHash 
-        
+
+
     def inserir(self):
+        aux2 = 0
         print('chave hash dentro do inserir',self.chaveHash)
         for cont in range(0,4): 
-            #print('entrei aqui ')
             if matriz[self.chaveHash][cont] == -1: # se tiver elemento neste lugar
                 matriz[self.chaveHash][cont] = self.chave
-            #matriz[self.chaveHash][:]= self.chave 
                 print('inserido na matriz',matriz[:][:])
                 break
             else:
                 #### continuar daki 
-                print('não posso inserir')
+                aux2 = aux2 + 1 ###incremento
+                if aux2 == 4:   ## se o bucket encheu 
+
+                    matriz[self.chaveHash][:] 
+                    print('encheu!!')
+
+                #print('não posso inserir')
 
 
         
@@ -31,14 +36,9 @@ while condicao == "s":
 
     aux = int(input('Digite a chave: '))
     objeto1 = Hashlinear(aux)  ## instanciei um objeto através do init
-#objeto2 = Hashlinear(24)
 
     objeto1.funcaoHash()
-#objeto2.funcaoHash()
+
     objeto1.inserir()
     condicao = input('Deseja inserir mais? ')
-#objeto2.inserir()
 
-#print('testeaqui',matriz[3][0])
-#aux = int(input('digite a chave a ser buscada: '))
-#print(Hashlinear.funcaoHash(aux))
